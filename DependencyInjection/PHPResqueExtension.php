a93a91675e051e4d5e4e7c3f241fdf02008a8fa4
Hélio Costa e Silva <helio.costa@vexcorp.com>
2011-09-16 18:07:58 -0300

    Configuration setup

--- DependencyInjection/PHPResqueExtension.php
+++ DependencyInjection/PHPResqueExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace hlegius\PHPResqueBundle\DependencyInjection;
+namespace PHPResqueBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class hlegiusPHPResqueExtension extends Extension
+class PHPResqueExtension extends Extension
 {
     /**
      * {@inheritDoc}
